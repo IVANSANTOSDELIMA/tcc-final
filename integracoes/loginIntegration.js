846b95da27d9b81b1c7a9fd190eb53e7297bf3a1
“justino1806” <54010279+justino1806@users.noreply.github.com>
2025-04-06 14:57:53 -0300

    feat: Implementa autenticação, integra dashboard com o backend; atualiza estilos e corrige títulos

--- integracoes/loginIntegration.js
+++ integracoes/loginIntegration.js
@@ -1,6 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
     const loginForm = document.getElementById("loginForm");
     const baseUrl = 'http://localhost:3000';
+
+    // Trecho responsavel para ver se o token é válido, e caso seja, levar para o dashboard
+    const token = localStorage.getItem('token');
+    if (token) {
+        const response = await fetch(`${baseUrl}/api/auth/verificar-token`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }); console.log(token);
+        if (response.ok) {
+            console.log('Token valido');
+            window.location.href = '/telainicio.html';
+        }
+    }
 
     loginForm.addEventListener("submit", async function (event) {
         event.preventDefault();
@@ -24,6 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
             if (response.ok) {
                 alert('Login realizado com sucesso!');
                 localStorage.setItem('token', data.token);
+                localStorage.setItem('funcionario', JSON.stringify(data.funcionario));
                 window.location.href = '/telainicio.html';
             } else {
                 alert(data.message || 'Erro ao realizar login');
